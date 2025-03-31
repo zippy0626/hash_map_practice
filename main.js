781bd07885bc1919fdc941ca3530282015adba6c
zippy0626 <zippy0626@gmail.com>
2025-03-31 12:06:08 -0400

    add dummy content

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import HashMap from "./hashMap.js";
 
 const hmap = new HashMap(16);
-console.log(hmap.hash("Rama"))
-console.log(hmap.hash("Sita"))
+
+hmap.set("too", "Hello");
+hmap.set("oot", "Hi");
+hmap.set("cat", "Meow");
+hmap.set("act", "Purr");
+hmap.set("dog", "Bark");
+hmap.set("gob", "Woof");
+hmap.set("bat", "Fly");
+hmap.set("tab", "Glide");
+hmap.set("tip", "Point");
+hmap.set("pit", "Drop");
+hmap.set("nap", "Rest");
+hmap.set("pan", "Cook");
+
+console.log(hmap.buckets)
+console.log(hmap.length())
+console.log(hmap.keys())
+console.log(hmap.values())
+console.log(hmap.entries())
